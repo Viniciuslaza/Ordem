dccde0a24b4504c26e8d20a51160c8dbcb1fd48a
Viniciuslaza <viniciuslazarinipecchia@gmail.com>
2020-09-14 21:02:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ordems/ActivitySplash.java
@@ -0,0 +1,14 @@
+package com.example.ordems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/example/ordems/ConsultaActivity.java
@@ -0,0 +1,60 @@
+package com.example.ordems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ConsultaActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private TextView tv_cliente, tv_atendente, tv_infe, tv_tipo,
+                     tv_marca, tv_modelo, tv_cor, tv_ns, tv_inf, v_laudo;
+
+    private Button btn_voltar;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta);
+
+
+        this.btn_voltar = findViewById(R.id.btn_voltar);
+        this.btn_voltar.setOnClickListener(this);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_voltar:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+        }
+
+    }
+}
